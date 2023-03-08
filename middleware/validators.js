63144754b83552d31a8f2d52d36ecd627d523c62
Andrei <nicolaev.andrei09@gmail.com>
2023-03-08 19:53:33 +0200

    body

--- middleware/validators.js
+++ middleware/validators.js
@@ -1,15 +1,15 @@
 module.exports.validateAngajat = (req, res, next) => {
   if (
-    req.nume.length >= 3 &&
-    req.prenume.length >= 3 &&
-    req.email.length >= 8 &&
-    req.salariu >= 0 &&
-    req.data.length >= 8
+    req.body.nume.length >= 3 &&
+    req.body.prenume.length >= 3 &&
+    req.body.email.length >= 8 &&
+    req.body.salariu >= 0 &&
+    req.body.data.length >= 8
   )
     next();
   else res.status(400).send("Eroare");
 };
 module.exports.validateFirma = (req, res, next) => {
-  if (req.nume.length >= 6) next();
+  if (req.body.nume.length >= 6) next();
   else res.status(400).send("Eroare");
 };
